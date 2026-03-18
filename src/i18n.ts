487d803a458d7f11e62341127117cb5dffb392ae
Omar E <31523746+omar-estevez@users.noreply.github.com>
2026-03-18 09:39:01 -0500

    test 2

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,7 @@ export type Lang = 'en' | 'es'
 export const translations = {
   en: {
     nav: {
-      howItWorks: 'How It Works',
+      howItWorks: 'How It Works aaaaa',
       parts: 'Parts',
       reviews: 'Reviews',
       phone: '+1 (832)-449-9104',
